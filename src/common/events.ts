978acea9c1eafb993a6d23bade807d76673bf1bb
Aditya Patange <contact.adityapatange@gmail.com>
2024-09-03 05:01:16 +0530

    events: add system error

--- src/common/events.ts
+++ src/common/events.ts
@@ -1,5 +1,6 @@
 enum SimulatorEvents {
   SYSTEM_LOG = "simulator:log",
+  SYSTEM_ERROR = "simulator:error",
   SIMULATOR_START = "simulator:start",
   SIMULATOR_STOP = "simulator:stop",
   SIMULATOR_PAUSE = "simulator:pause",
