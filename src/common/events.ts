2867e5886b8282532199743e3cbf0f302bbe53d2
Aditya Patange <contact.adityapatange@gmail.com>
2024-09-03 04:06:22 +0530

    common: add events

---
+++ src/common/events.ts
@@ -0,0 +1,9 @@
+enum SimulatorEvents {
+  SYSTEM_LOG = "simulator:log",
+  SIMULATOR_START = "simulator:start",
+  SIMULATOR_STOP = "simulator:stop",
+  SIMULATOR_PAUSE = "simulator:pause",
+  SIMULATOR_RESUME = "simulator:resume",
+}
+
+export { SimulatorEvents };
