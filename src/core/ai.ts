b04ae7b033831697f9b616d4d35f3f0c0e65f39c
Aditya Patange <contact.adityapatange@gmail.com>
2024-09-03 05:01:06 +0530

    ai: add json mode and use simulator logger

--- src/core/ai.ts
+++ src/core/ai.ts
@@ -1,4 +1,6 @@
 import axios from "axios";
+import { simulatorLogger } from "./logger";
+import { SimulatorEvents, Utils } from "../common";
 
 const API_URL = "http://localhost:1234/v1/chat/completions";
 
@@ -7,51 +9,77 @@ async function llm({
   prompt,
   temperature,
   maxTokens,
+  json,
 }: {
   system?: string;
   prompt: string;
   temperature?: number;
   maxTokens?: number;
-}) {
-  if (!system) {
-    system =
-      "You are a helpful assistant that helps people with their problems.";
-  }
-
-  if (!prompt) {
-    throw new Error("Prompt required. Please provide a prompt.");
-  }
-
-  if (!temperature) {
-    temperature = 0.5;
-  }
-
-  if (!maxTokens) {
-    maxTokens = 4096;
-  }
-
-  const url = API_URL;
-  const data = {
-    model: "LM Studio Community/Meta-Llama-3-8B-Instruct-GGUF",
-    messages: [
-      { role: "system", content: system },
-      { role: "user", content: prompt },
-    ],
-    temperature: temperature,
-    max_tokens: maxTokens,
-    stream: false,
-  };
-
+  json?: boolean;
+}): Promise<any> {
   try {
+    if (json == undefined) {
+      json = true;
+    }
+
+    if (!system) {
+      system =
+        "You are a helpful assistant that helps people with their problems.";
+    }
+
+    if (!prompt) {
+      throw new Error("Prompt required. Please provide a prompt.");
+    }
+
+    if (!temperature) {
+      temperature = 0.5;
+    }
+
+    if (!maxTokens) {
+      maxTokens = 4096;
+    }
+
+    const url = API_URL;
+    const data = {
+      model: "LM Studio Community/Meta-Llama-3-8B-Instruct-GGUF",
+      messages: [
+        { role: "system", content: system },
+        {
+          role: "user",
+          content: prompt + json ? "\nReturn the response as JSON" : "",
+        },
+      ],
+      temperature: temperature,
+      max_tokens: maxTokens,
+      stream: false,
+    };
+
     const response = await axios.post(url, data, {
       headers: {
         "Content-Type": "application/json",
       },
     });
 
-    return response.data.choices[0].message.content;
+    const rawResult = response.data.choices[0].message.content;
+
+    if (json) {
+      const cleanedJson = Utils.cleanGPTJson(
+        response.data.choices[0].message.content
+      );
+      const parsedJson = Utils.parseJSON(cleanedJson);
+      return parsedJson;
+    }
+
+    return rawResult;
   } catch (error) {
-    console.error("Error making the request:", error);
+    simulatorLogger.log({
+      event: SimulatorEvents.SYSTEM_ERROR,
+      message: "Error with AI completion.",
+      data: {
+        error,
+      },
+    });
+    return null;
   }
 }
 
