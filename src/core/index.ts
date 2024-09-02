ed80519f5aa1b8d31cf2e69cc187d0e6c1cbfe61
Aditya Patange <contact.adityapatange@gmail.com>
2024-09-03 02:24:49 +0530

    core: simulator base execution functions

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,1 +1,1 @@
-export {};
+export { Simulator } from "./simulator";
--- src/core/simulator.ts
+++ src/core/simulator.ts
@@ -8,6 +8,9 @@ import { SimulatorOptions } from "../models/options/simulator.options";
 
 class Simulator {
   private options: SimulatorOptions;
+  private logs: string[] = [];
+  private isRunning: boolean = false;
+  private isPaused: boolean = false;
 
   constructor(options: SimulatorOptions) {
     this.options = this.initOptionsWithDefaults(options);
@@ -21,6 +24,9 @@ class Simulator {
    */
   initOptionsWithDefaults(options: SimulatorOptions): SimulatorOptions {
     return {
+      name: options.name,
+      description:
+        options.description || "a generic simulation with nothing special",
       iterations: options.iterations || 1000,
       numSentients: options.numSentients || 10,
       numNonSentients: options.numNonSentients || 10,
@@ -33,4 +39,68 @@ class Simulator {
       data: options.data || [],
     };
   }
+
+  /**
+   * Starts the simulation.
+   */
+  start(): void {
+    this.isRunning = true;
+    console.log(`SIMULATION '${this.options.name}' started.`);
+  }
+
+  /**
+   * Stops the simulation.
+   */
+  stop(): void {
+    this.isRunning = false;
+    let message = `SIMULATION '${this.options.name}' stopped.`;
+    this.log(message);
+  }
+
+  /**
+   * Pauses the simulation.
+   */
+  pause(): void {
+    this.isPaused = true;
+    const message = `SIMULATION '${this.options.name}' paused.`;
+    this.log(message);
+  }
+
+  /**
+   * Resumes the simulation.
+   */
+  resume(): void {
+    this.isPaused = false;
+    const message = `SIMULATION '${this.options.name}' resumed.`;
+    this.log(message);
+  }
+
+  /**
+   * Logs a message.
+   *
+   * @param message Message to log.
+   */
+  log(message: string): void {
+    this.logs.push(message);
+    console.log(message);
+  }
+
+  /**
+   * Gets the logs.
+   *
+   * @returns Logs.
+   */
+  getLogs(): string[] {
+    return this.logs;
+  }
+
+  /**
+   * Helper function to get the running state of the simulation.
+   * @returns Running state of the simulation.
+   */
+  getRunningState(): boolean {
+    return this.isRunning && !this.isPaused;
+  }
 }
+
+export { Simulator };
